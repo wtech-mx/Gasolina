87a8c78420357bf123ad1600b137457502d55f91
verplancken <42614785+verplancken@users.noreply.github.com>
2022-01-24 09:53:07 -0600

    vista de sucirsaales

--- resources/views/admin/sucursales/create.blade.php
+++ resources/views/admin/sucursales/create.blade.php
@@ -1,25 +1,77 @@
-<form method="POST" action="{{ route('store.sucursal') }}" enctype="multipart/form-data" role="form">
-    @csrf
-    <div class="mb-3">
-        <label class="form-label">Nombre</label>
-        <input type="text" class="form-control" id="name" name="name">
-      </div>
-    <div class="mb-3">
-      <label class="form-label">Correo</label>
-      <input type="email" class="form-control" id="email" name="email">
-    </div>
-    <div class="mb-3">
-      <label class="form-label">Contraseña</label>
-      <input type="password" class="form-control" id="password" name="password">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Empresa</label>
-        <select class="form-select" aria-label="Default select example">
-            @foreach ($empresa as $item)
-                <option value="{{$item->id}}">{{$item->name}}</option>
-            @endforeach
-        </select>
-    </div>
+@extends('layouts.app')
 
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+@section('content')
+
+
+<div class="box">
+    <div class="row">
+        <div class="col-12 mt-3">
+            <div class="d-flex justify-content-between p-3">
+
+                <button class="btn btn-back" type="button">
+                    <i class="fa fa-arrow-circle-o-left btn-icon-back"></i>
+                </button>
+
+                <h1 class="text-white">Pagina</h1>
+
+                <h1 class="text-white">-</h1>
+
+            </div>
+        </div>
+
+        <div class="col-12 p-4">
+            <form method="POST" action="{{ route('store.sucursal') }}" enctype="multipart/form-data" role="form">
+                @csrf
+                <div class="row">
+                    <div class="col-sm-6 col-6">
+                        <div class="form-group mb-3 position-relative">
+                            <i class="fa fa-user icon-style-2"></i>
+                            <label class="form-label label-custom-yellow">Nombre</label>
+                            <input type="text" class="form-control input-style" id="name" name="name" placeholder="Nombre">
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-6">
+                        <div class="form-group mb-3 position-relative">
+                            <i class="fa fa-font icon-style-2"></i>
+                            <label class="form-label">Correo</label>
+                            <input type="email" class="form-control input-style" id="email" name="email" placeholder="Correo">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6 col-6">
+                        <div class="form-group mb-3 position-relative">
+                            <i class="fa fa-lock icon-style-2"></i>
+                            <label class="form-label label-custom-yellow">Contraseña</label>
+                            <input type="password" class="form-control input-style" id="password" name="password" placeholder="Contraseña">
+                        </div>
+                    </div>
+
+
+                    <div class="col-sm-6 col-6">
+                        <div class="form-group mb-3 position-relative">
+                            <i class="fa fa-lock icon-style-2"></i>
+                            <label class="form-label">Empresa</label>
+                            <select class="form-select input-style" aria-label="Default select example" id="id_empresa" name="id_empresa">
+                                @foreach ($empresa as $item)
+                                    <option value="{{$item->id}}">{{$item->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <button class="btn-saved" type="submit">
+                            <i class="fa fa-save"></i>Guardar
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
