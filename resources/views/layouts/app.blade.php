8ed252cd470ad628a186a34e3324d1ff7bc7fc7c
verplancken <42614785+verplancken@users.noreply.github.com>
2022-02-09 22:36:51 -0600

    revert livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,93 +3,50 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<title>@hasSection('title') @yield('title') | @endif {{ config('app.name', 'Laravel') }}</title>
+    <!-- Css -->
+    <link rel="icon"       href="{{ asset('images/icons/icon-72x72.png') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/buttons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/forms.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/menu.css') }}">
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
+>
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/font-awesome.min.css') }}">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-	 @livewireStyles
+
+    <!-- Boostrap -->
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+
+     @yield('css')
+     @yield('fullcalendar')
+     @yield('scripts')
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-					@auth()
-                    <ul class="navbar-nav mr-auto">
-						<!--Nav Bar Hooks - Do not delete!!-->
-						<li class="nav-item">
-                            <a href="{{ url('/actividades') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Actividades</a> 
-                        </li>
-                    </ul>
-					@endauth()
-					
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                                <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-2">
+<body class="body-bg">
             @yield('content')
-        </main>
-    </div>
-    @livewireScripts
-<script type="text/javascript">
-	window.livewire.on('closeModal', () => {
-		$('#createDataModal').modal('hide');
-	});
-</script>
+
+        @auth
+            <div class="btn-menu-fixed">
+                <a class="btn btn-floating" role="button" data-bs-toggle="modal" data-bs-target="#modal-menu">
+                    <i class="fa fa-bars text-dark"></i>
+                </a>
+            </div>
+            {{--modal--}}
+
+            @include('layouts.menu')
+        @endauth
+
 </body>
+
+<!-- Boostrap -->
+
+    <script src={{ asset('assets/bootstrap/js/bootstrap.min.js') }}></script>
+    @include('sweetalert::alert')
+
 </html>
