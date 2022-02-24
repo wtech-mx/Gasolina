835155900aabf4a6346f5bf9bdece2220e459a31
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-02-24 11:40:00 -0600

    Actualizacion de modal calendar

--- resources/views/fullcalendar/modal.blade.php
+++ resources/views/fullcalendar/modal.blade.php
@@ -1,14 +1,18 @@
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
+      <div class="modal-content" style="border-radius: 30px;">
 
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Datos del Evento</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="modal-body" style="border-radius: 30px!important;border: 2px solid #FFC300;background-color: #001d3d;">
+
+        <div class="d-flex justify-content-between">
+
+            <h5 class="modal-title mb-5 text-white" id="exampleModalLabel">Datos del Evento</h5>
+
+             <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close" style="background: transparent url({{asset('assets/img/icons/cancel.png') }}) center/1.5em auto no-repeat;">
+             </button>
+
         </div>
-
-        <div class="modal-body">
 
             <div class="form-row">
 
@@ -17,7 +21,7 @@
                 </div>
 
                 <div class="form-group col-12">
-                  <label for="">Fecha</label>
+                  <label class="text-white" for="">Fecha</label>
                   <input class="form-control" type="date" name="txtFecha" id="txtFecha">
                 </div>
 
@@ -34,15 +38,15 @@
                 </div>
 
                 <div class="form-group col-12 mb-3">
-                    <label for="">Título</label>
+                    <label class="text-white" for="">Título</label>
                     <input class="form-control" type="text" name="txtTitle" id="txtTitle" >
                 </div>
 
-                <label for="">Description</label>
+                <label class="text-white" for="">Description</label>
                 <textarea class="form-control" name="descripcion" id="descripcion" cols="30" rows="3"></textarea>
 
                 <div class="form-group col-12 mt-3">
-                    <label for="">¿Esta tarea ya esta realizada?</label>
+                    <label class="text-white" for="">¿Esta tarea ya esta realizada?</label>
 
                     <select class="form-control" id="check" name="check">
                         <option value="0">Pendiente</option>
@@ -61,22 +65,23 @@
                 @endswitch
             @endforeach --}}
 
-
-        </div>
-
-        <div class="modal-footer">
-            <button class="btn btn-success text-white" id="btnAgregar">
+            <div class="d-flex justify-content-around mt-5">
+            <button class="btn  " id="btnAgregar" style="background-color:#001d3d;color:  #41CC2E!important; border: 2px solid #41CC2E!important;">
                 <i class="fa fa-plus-circle" aria-hidden="true"></i> Agregar
             </button>
 
-            <button class="btn btn-warning text-dark" id="btnModificar">
+            <button class="btn  text-dark " id="btnModificar" style="background-color:#001d3d;color:  yellow!important; border: 2px solid yellow!important;">
                 <i class="fa fa-retweet" aria-hidden="true"></i> Modificar
             </button>
 
-            <button class="btn btn-danger text-white" id="btnBorrar">
+            <button class="btn  " id="btnBorrar" style="background-color:#001d3d;color:  red!important; border: 2px solid red!important;">
                 <i class="fa fa-trash" aria-hidden="true"></i> Borrar
             </button>
+            </div>
+
         </div>
+
+
 
       </div>
     </div>
