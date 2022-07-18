ca7cea9fbc042191811bf6693b83be9fefe9e636
verplancken <42614785+verplancken@users.noreply.github.com>
2022-07-18 16:30:09 -0500

    documento

---
+++ resources/views/modal-elementos/formularios xii/pdf_xii_01.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.pdf.layout_pdf')
+
+@section('template_title')
+    XII
+@endsection
+
+@section('content_pdf')
+            <div class="col-12 ">
+                <p class="p-5">
+                <strong>1.- OBJETIVO</strong><br><br>
+                1.1.- Disponer de las medidas de control, con respuesta inmediata de protección tanto al recurso
+                humano como al recurso material, ante contingencias naturales o las originadas por acciones del ser
+                humano, que comprometan la integridad física, la continuidad de los trabajos y la seguridad de las
+                instalaciones o el Medio Ambiente. Restablecer los trabajos a corto tiempo y con pérdidas mínimas
+                aceptables.. <br><br>
+                <strong>2.- ALCANCE</strong><br><br>
+                2.1.- Es de alcance para todo el personal y trabajadores que tengan conocimientos en primeros
+                auxilios, rescate y aquellos que han participado en bomberos, brigadas de emergencia, etc.
+                Mantener preparada la Estación de Servicio para asegurar una respuesta oportuna ante cualquier
+                situación de emergencia, utilizando los recursos disponibles. <br><br>
+                <strong>3.- RESPONSABILIDADES Y DESARROLLO</strong><br><br>
+                3.1.- Brigada de emergencias multifuncional. <br>
+                La brigada estará compuesta por personal de la Estación de Servicio, utilizando el acta de
+                conformación de brigada, Los integrantes de las brigadas de emergencia, serán personas a las que se
+                capacitará interna mente con el curso Atención a emergencias "Brigada Multifuncional" que se
+                encuentra dentro de la herramienta GASAFE o capacitación externa mediante un tercero acreditado
+                como agente capacitador externo ante STPS y Protección Civil en materia de primeros auxilios,
+                combate de incendios, búsqueda y rescate, evacuación, respuesta ante emergencias ambientales,
+                (derrames, fugas) y habrá representantes de la brigada durante toda la jornada laboral o los diferentes
+                turnos. El tamaño de la brigada, dependerá de la cantidad de personas disponibles que se encuentren
+                en la Estación de Servicio. <br>
+                Se deben de tener en cuenta los turnos, ya que siempre tienen que estar presentes brigadistas en los
+                turnos de la Estación de Servicio. <br>
+                3.2.- Responsabilidades de los integrantes de la brigada: <br><br>
+                - Emitir la señal de alarma. <br>
+                - Asistir a los cursos programados de brigadistas y conocer los procesos de respuesta a
+                emergencias. <br>
+                - Conocer la ubicación de los equipos de atención a emergencia y rutas de evacuación (croquis de
+                seguridad). <br>
+                - Participar en cualquier atención a emergencia y simulacro. <br>
+                - Participar en cualquier actividad de revisión de equipo de emergencia que sea asignado por el
+                dueño de este procedimiento (jefe de brigada). <br>
+
+                </p>
+            </div>
+
+            @include('layouts.pdf.pdf_header')
+
+            <div class="col-12 ">
+                <p class="p-5">
+                   <strong> 3.2.1.- Jefe de brigada.</strong> <br><br>
+                    Funciones y responsabilidades: <br>
+                    - Coordinar todas las actividades relacionadas con la atención a emergencias en base a los
+                    documentos establecidos en el procedimiento de preparación y respuesta a emergencias con
+                    código XIII. <br>
+                    - Mantener organizada la brigada de la estación. <br>
+                    - Prever que se cuente con el equipo de seguridad, de comunicación y material de curación
+                    necesario para hacerle frente a cualquier contingencia (botiquín). <br>
+                    - Actuar de forma oportuna, adecuada, coordinada y eficiente. <br>
+                    - Mantener contacto continuo con los brigadistas durante la emergencia. <br>
+                    - Solicitar ayuda exterior cuando sea necesario (Bomberos, Cruz Roja, Seguridad Pública, Tránsito,
+                    etc.). <br>
+                    - Ordenar el traslado de lesionados a hospitales. <br>
+                    - Ordenar el desalojo total del personal de la empresa. <br>
+                    - Gestionar la integridad física de los brigadistas y de los cuerpos auxiliares externos. <br>
+                    - Advertir a los brigadistas sobre el peligro real del siniestro. <br>
+                    - Observar y auxiliar de forma continúa a las brigadas y cuerpos de auxilio externos. <br>
+                    - Proporcionar toda clase de información a las instituciones de auxilio del proceso y materiales que
+                    se manejan dentro del mismo. <br>
+                    - Realizar el corte de luz eléctrica y cierre de suministro de gas o cualquier combustible. <br>
+                    - Ubicar de manera provisional el puesto de primeros auxilios, en una zona segura y fuera de la
+                    zona de riesgo. <br>
+
+                </p>
+            </div>
+
+
+
+            @include('layouts.pdf.pdf_header')
+
+            <div class="col-12 ">
+                <p class="p-5">
+                   <strong> 3.2.2.- Brigadistas.</strong> <br><br>
+                    Funciones y responsabilidades: <br>
+                    - Seguir las indicaciones del jefe de brigada. <br>
+                    - Recolectar el equipo de seguridad y de extinción utilizado por una emergencia o simulacro. <br>
+                    - Esperar indicaciones del jefe de brigada. <br>
+                    - Apoyar en la vuelta a la normalidad <br> <br>
+                    <strong>3.3.- Creación de brigada</strong>
+                    El responsable del elemento XIII, deberá de crear el acta de conformación de brigada, siguiendo los
+                    siguientes pasos: <br>
+                    isional el puesto de primeros auxilios, en una zona segura y fuera de la
+                    zona de riesgo. <br>
+                    - Deberá entrar al elemento del sistema XIII y deberá dar clic en el formato de resgistro Acta de
+                    conformación de brigada. <br>
+                    - Dará clic en la actividad ejecutar. <br>
+                    - Llenará la información general. <br>
+                    - Llenará el personal de brigada por cada turno de la Estación de Servicio
+                    El responsable del elemento XIII, deberá de actualizar anualmente el acta de conformación de brigada,
+                    tomando encuenta el presente procedimiento y ejecutando las actividades dentro de la Herramienta
+                    Gasafe. <br>
+                    <strong>4.- REGISTROS</strong> <br><br>
+                    4.1.- Acta de conformación de brigada.
+
+
+                </p>
+            </div>
+
+
+@endsection
+
+
